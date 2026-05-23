37b596cd98411bb06d7a2dddcc75bee3ef3dd044
vietanh-io <vietanh.wp25@gmail.com>
2026-05-24 00:47:19 +0700

    Set up backend nodejs

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import markdown from '@eslint/markdown'
+import { defineConfig } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+export default defineConfig([
+  { ignores: ['**/.agents/**', '**/node_modules/**', '**/dist/**', '**/generated/**', '**/src/db/generated/**'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node }
+  },
+  tseslint.configs.recommended,
+
+  {
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    },
+    ignores: ['**/.agents/**', '**/node_modules/', '**/dist/', '**/generated/', '**/src/db/generated/']
+  },
+
+  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] }
+])
